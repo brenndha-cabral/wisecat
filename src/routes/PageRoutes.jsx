35ccd7e8f1538684759a987dcb47b4ce77745a97
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-04-20 21:52:45 -0300

    :sparkles: feat: Creating route file

---
+++ src/routes/PageRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PageHome from '../pages/home/PageHome';
+import PageAdvices from '../pages/advices/PageAdvices';
+import PageNotFound from '../pages/notFound/PageNotFound';
+
+function PageRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<PageHome />} />
+        <Route exact path="/advices" element={<PageAdvices />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default PageRoutes;
