5712c778fabd22a215bf571ef648b97e7c33766f
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-04-20 21:16:25 -0300

    :construction: feat: Creating Home and Advices pages | :wrench: build: Changing .editorconfig

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>
+        Wise
+        <span>Cat</span>
+      </h1>
+      <button
+        type="button"
+      >
+        Ask for advice
+      </button>
+    </div>
+  );
+}
+
+export default Home;
