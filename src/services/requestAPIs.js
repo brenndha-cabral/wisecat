f7638b3c2abd5436c9e2ff8fa1f4f1be5b024ccf
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-04-21 19:12:43 -0300

    :sparkles: feat: Creating requests API

---
+++ src/services/requestAPIs.js
@@ -0,0 +1,21 @@
+export const requestAdvices = async () => {
+  try {
+    const URL = 'https://api.adviceslip.com/advice';
+    const response = await fetch(URL);
+    const { advice } = await response.json();
+    return advice;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+export const requestCatsImages = async () => {
+  try {
+    const URL = 'https://api.thecatapi.com/v1/images/search';
+    const response = await fetch(URL);
+    const { url } = await response.json();
+    return url;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
