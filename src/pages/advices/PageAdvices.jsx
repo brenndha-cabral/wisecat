e54185f32c55e2ae4a3f2c29f8abcba0386e3535
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-04-26 23:08:59 -0300

    :sparkles: fix: Souving rendering asynchronicity

--- src/pages/advices/PageAdvices.jsx
+++ src/pages/advices/PageAdvices.jsx
@@ -1,27 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import handleAdviceAndCatImage from '../../helpers/handleAdviceAndCatImage';
 import myContext from '../../contextAPI/myContext';
-import './pageAdvices.css';
+import '../../style/pages.css';
 
-function Advices() {
+function PageAdvices() {
   const { handleInfos, setHandleInfos } = useContext(myContext);
+
+  const [uploadedImage, setUploadedImage] = useState(false);
+
+  const handleImageLoaded = () => {
+    setUploadedImage(true);
+  };
 
   return (
     <main>
-      <button
-        type="button"
-        onClick={async () => {
-          setHandleInfos(await handleAdviceAndCatImage());
-        }}
-      >
-        Next advice
-      </button>
-      <div>
-        <h1>{ handleInfos.advice }</h1>
-        <img src={handleInfos.catImage} alt="Cat" />
-      </div>
+      <section className="container">
+        <h1>
+          Wise
+          <span>Cat</span>
+        </h1>
+        <button
+          type="button"
+          onClick={async () => {
+            setUploadedImage(false);
+            setHandleInfos({ catImage: '' });
+            setHandleInfos(await handleAdviceAndCatImage());
+          }}
+        >
+          Next advice
+        </button>
+        <div className="content">
+          { uploadedImage && <h1>{ handleInfos.advice }</h1> }
+          { handleInfos.catImage
+          && (
+          <img
+            className="cat-image"
+            src={handleInfos.catImage}
+            alt="Cat"
+            onLoad={handleImageLoaded}
+          />
+          )}
+        </div>
+      </section>
     </main>
   );
 }
 
-export default Advices;
+export default PageAdvices;
